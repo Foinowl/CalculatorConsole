47241cbe14fa03a4ee216e3fcf0785cc31e6a3ce
Evgeniy <nifpopnaf@gmail.com>
2021-11-03 09:29:01 +0400

    add main methods to CalculatorServiceImpl

--- src/com/company/service/CalculatorServiceImpl.java
+++ src/com/company/service/CalculatorServiceImpl.java
@@ -1,14 +1,15 @@
 package com.company.service;
 
+import com.company.lexeme.Brackets;
 import com.company.lexeme.Lexeme;
+import com.company.lexeme.Operator;
 
-import java.util.List;
+import java.util.*;
 
 public class CalculatorServiceImpl implements CalculatorService {
 
     private final String line;
 
-    private String postfixNotationExpression;
     private ParserService parserService = new ParserServiceImpl();
 
     public CalculatorServiceImpl(String line) {
@@ -18,26 +19,37 @@ public class CalculatorServiceImpl implements CalculatorService {
     public String execute() {
         List<Lexeme> listLexeme = parserService.parse(this.line);
 
-        setPostfixNotationExpression(buildPostfixNotation(listLexeme));
-        return "2";
+        List<Lexeme> listPostfixNotation = buildPostfixNotation(listLexeme);
+        return String.valueOf(calculatePostfixNotation(listPostfixNotation));
     }
 
-    private String buildPostfixNotation(final List<Lexeme> expressionLexeme) {
+    private List<Lexeme> buildPostfixNotation(final List<Lexeme> expressionLexeme) {
 //        Let's start a stack, initially it is empty. We will move from left to right according to the expression in reverse Polish notation;
 //        if the current element is a number or variable, then we put its value on the top of the stack;
 //        if the current element is an operation, then we take out the top two elements from the stack (or one if the operation is unary),
 //        apply the operation to them, and put the result back on the stack.
 //        In the end, there will be exactly one element left in the stack - the value of the expression.
+        List<Lexeme> result = new ArrayList<>();
+        Deque<Lexeme> stack = new ArrayDeque<>();
+
+        for (Lexeme lexeme : expressionLexeme) {
+            if (lexeme instanceof Operator) {
+//                TODO: take out the top two values from stack, if operation is not unary
+            } else if (lexeme instanceof Brackets) {
+                if (((Brackets) lexeme).isOpen()) {
+                    stack.addFirst(lexeme);
+                } else {
+                    // pop from stack until first open bracket
+                }
+            } else {
+                result.add(lexeme);
+            }
+        }
+
         return null;
     }
 
-
-    private String getPostfixNotationExpression() {
-        return postfixNotationExpression;
+    private double calculatePostfixNotation(List<Lexeme> listExpression) {
+        return 0.0;
     }
-
-    private void setPostfixNotationExpression(String postfixNotationExpression) {
-        this.postfixNotationExpression = postfixNotationExpression;
-    }
-
 }
